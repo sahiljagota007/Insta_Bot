8aa758b5a12abf45dbc06ad7913413787aacf38b
sahil jagota <sahiljagota.100.sj@gmail.com>
2017-07-17 10:29:32 +0530

    accesss rtoken

---
+++ constants.py
@@ -0,0 +1,7 @@
+#global var files
+
+#instagram api access token
+APP_ACCESS_TOKEN= '2965012272.3b15da3.38a74af103e74a2f9814f59126e99416'
+
+#instagram api base url
+BASE_URL= 'https://api.instagram.com/v1/'
